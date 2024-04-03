7f63fbb4079046f8c8076cc028336982d8b0f753
Rishit Hemant Thakkar <rht2122@columbia.edu>
2024-04-03 16:58:08 +0000

    Added part6 of hls4ml and ran build for testing for the full pipeline

---
+++ pruned_cnn/firmware/weights/b26.h
@@ -0,0 +1,15 @@
+//Numpy array shape [10]
+//Min -0.052268479019
+//Max 0.080548837781
+//Number of zeros 0
+
+#ifndef B26_H_
+#define B26_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b26[10];
+#else
+output_dense_bias_t b26[10] = {-0.0421250798, -0.0020485015, 0.0088895662, 0.0751619264, -0.0522684790, 0.0339110307, 0.0805488378, -0.0336558707, -0.0148231396, 0.0793913156};
+#endif
+
+#endif
---
+++ quantized_pruned_cnn/firmware/weights/b23.h
@@ -0,0 +1,15 @@
+//Numpy array shape [10]
+//Min -0.090050891042
+//Max 0.133798032999
+//Number of zeros 0
+
+#ifndef B23_H_
+#define B23_H_
+
+#ifndef __SYNTHESIS__
+output_dense_bias_t b23[10];
+#else
+output_dense_bias_t b23[10] = {-0.0291783623, -0.0318061523, -0.0361037590, 0.0704882964, 0.0290045440, -0.0016560076, 0.1337980330, -0.0900508910, 0.0693566203, -0.0253325086};
+#endif
+
+#endif
